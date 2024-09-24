7a11d57293178fea221195a4319434d061218f46
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2024-09-24 23:02:27 +0530

    [58] [Time Beats: 47.40%] [Memory Beats: 66.77%] - LeetPush

---
+++ 58-Length-of-Last-Word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        for(int i = s.length() - 1; i >= 0; i--)
+        {
+            if(s[i] == ' ' && len == 0)
+            continue;
+
+            else if(s[i] == ' ')
+            break;
+
+            len++;
+        }
+
+        return len;
+    }
+};
