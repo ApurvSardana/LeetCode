1245d1673148dc88aaee3e9419b4cd587d7cd6d5
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2024-10-02 19:00:57 +0530

    [2089] [Time Beats: 65.43%] [Memory Beats: 10.12%] - LeetPush

---
+++ 2089-Find-Target-Indices-After-Sorting-Array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == target)
+            {
+            while(i < nums.size() && nums[i]==target)
+            {
+            ans.push_back(i);
+            i++;
+            }
+            break;
+            }
+        }
+        return ans;
+    }
+};
