2abe06742a3b8fe7888a2dc08f19fe7feec047a3
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2024-10-05 14:46:22 +0530

    [387] [Time Beats: 48.73%] [Memory Beats: 30.05%] - LeetPush

---
+++ 387-First-Unique-Character-in-a-String.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map <char, int> m;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(m.find(s[i]) != m.end())
+            {
+                m[s[i]] ++;
+            }
+
+            else
+            m[s[i]] = 1;
+        }
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(m[s[i]] == 1)
+            return i;
+        }
+
+        return -1;
+    }
+};
