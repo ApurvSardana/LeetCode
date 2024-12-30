ed79c9a9ffc5ac67405da136f62e8794d27a8acd
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2024-12-30 17:25:43 +0530

    [210] [Time Beats: 44.66%] [Memory Beats: 28.99%] - LeetPush

--- 210-Course-Schedule-II.cpp
+++ 210-Course-Schedule-II.cpp
@@ -1,45 +1,50 @@
 class Solution {
 public:
-    bool flag = false;
-    void dfs(int s, vector<vector<int>> &adj, vector<bool> &visited, vector<int> &order, vector<bool> &beenThere)
-    {
-        beenThere[s] = true;
-
-        for(int i : adj[s])
-        {
-            if(beenThere[i] == true && visited[i] == false)
-            {
-                flag = true;
-                return;
-            }
-
-            if(!visited[i])
-                dfs(i, adj, visited, order, beenThere);
-        }
-
-        visited[s] = true;
-        order.push_back(s);
-    }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int> order;
         vector<bool> visited(numCourses, false);
-        vector<bool> beenThere(numCourses, false);
+
+        vector<int> inDegree(numCourses, 0);
+
         vector<vector<int>> adj(numCourses);
 
-        for(auto p : prerequisites)
-            adj[p[0]].push_back(p[1]);
-        
-        for(int i = 0; i < numCourses; i++)
+        for(auto node : prerequisites)
         {
-            if(visited[i] == true)
-                continue;
-            
-            else
-                dfs(i, adj, visited, order, beenThere);
-
-            if(flag == true)
-                return {};
+            adj[node[0]].push_back(node[1]);
+            inDegree[node[1]]++;
         }
+
+        queue<int> q;
+
+        for(int i = 0; i < numCourses; i++)
+            if(!visited[i] && inDegree[i] == 0)
+                q.push(i);
+
+        if(q.empty())
+            return {};
+        
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            visited[node] = true;
+
+            order.push_back(node);
+
+            for(int i : adj[node])
+            {
+                inDegree[i]--;
+                if(inDegree[i] == 0)
+                    q.push(i);
+            }
+        }
+
+        for(int i = 0; i < numCourses; i++)
+            if(!visited[i])
+                return {};
+
+        reverse(order.begin(), order.end());
+        
         return order;
     }
 };
