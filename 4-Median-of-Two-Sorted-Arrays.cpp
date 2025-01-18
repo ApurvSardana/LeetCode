23abf81bfc4c802b48c4addb04db4d58202e60cd
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-01-19 03:38:24 +0530

    [4] [Time Beats: 100.00%] [Memory Beats: 21.01%] - LeetPush

--- 4-Median-of-Two-Sorted-Arrays.cpp
+++ 4-Median-of-Two-Sorted-Arrays.cpp
@@ -2,20 +2,31 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         vector<int> nums;
-        for(int num : nums1)
-        nums.push_back(num);
+        int n = nums1.size();
+        int m = nums2.size();
+        int i = 0; int j = 0;
 
-        for(int num2 : nums2)
-        nums.push_back(num2);
+        while(i < n && j < m)
+        {
+            if(nums1[i] <= nums2[j])
+                nums.push_back(nums1[i++]);
 
-        sort(nums.begin(), nums.end());
+            else
+                nums.push_back(nums2[j++]);
+        }
 
-        int len = nums.size();
+            while(i < n)
+                nums.push_back(nums1[i++]);
+
+            while(j < m)
+                nums.push_back(nums2[j++]);
+
+        int len = n + m;
 
         if(len%2 != 0)
-        return nums[len/2];
+            return (double)nums[len/2];
 
         else
-        return (double)(nums[(len/2) - 1] + nums[len/2]) / 2;
+            return (double)(nums[(len/2) - 1] + nums[len/2]) / 2.0;
     }
 };
