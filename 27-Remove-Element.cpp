aa171266beb2ea43f76714bafe1c99b76df6d3c5
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-01-18 19:15:01 +0530

    [27] [Time Beats: 100.00%] [Memory Beats: 35.68%] - LeetPush

---
+++ 27-Remove-Element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int i = 0;
+        int j = n - 1;
+        int k = 0;
+
+        while(i <= j)
+        {
+            if(nums[i] == val)
+            {
+                swap(nums[i], nums[j]);
+                j--;
+            }
+
+            else
+            {
+                k++;
+                i++;
+            }
+        }
+        cout << k;
+        return k;
+    }
+};
