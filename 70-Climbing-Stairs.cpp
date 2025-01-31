e924fc69e766aad499e734ad2f4f91a5d06a9489
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-01-31 17:16:27 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 55.81%] - LeetPush

--- 70-Climbing-Stairs.cpp
+++ 70-Climbing-Stairs.cpp
@@ -1,55 +1,19 @@
 class Solution {
 public:
-typedef struct{
-    int m[2][2];
-}matrix;
-
-matrix mul(matrix a, matrix b)
-{
-    matrix c = {{
-        {0,0},
-        {0,0}
-    }};
-    for(int i = 0; i < 2; i++)
-    for(int j = 0; j < 2; j++)
-    for(int k = 0; k < 2; k++)
-    c.m[i][j] += a.m[i][k] * b.m[k][j];
-
-    return c;
-}
-
-matrix mpow(matrix a, int b)
-{
-    matrix I = {{
-        {1, 0},
-        {0, 1}
-    }};
-    if(b == 0) return I;
-
-    matrix ans = mpow(a, b/2);
-    ans = mul(ans, ans);
-    if(b % 2 != 0)
-    ans = mul(ans, a);
-
-    return ans;
-}
-
-
+    vector<int> dp;
+    Solution(){
+        dp.assign(46, 0);
+    }
     int climbStairs(int n) {
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        int y = 1;
-        int x = 1;
+        if(n == 1)
+            return dp[n] = 1;
 
-        matrix a = {{
-            {1, 1},
-            {1, 0}
-        }};
-
-        matrix p = mpow(a, n - 1);
-
-        return p.m[0][0] + p.m[0][1];
-
+        if(n == 2)
+            return dp[n] = 2;
         
+        if(dp[n] != 0)
+            return dp[n];
+        
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
     }
 };
