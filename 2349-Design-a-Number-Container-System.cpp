ab1ce03f0a6bec884ef43ee8b709c711d81ac108
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-02-08 22:38:11 +0530

    [2349] [Time Beats: 37.50%] [Memory Beats: 61.88%] - LeetPush

---
+++ 2349-Design-a-Number-Container-System.cpp
@@ -0,0 +1,35 @@
+class NumberContainers {
+public:
+    unordered_map <int, int> itn;
+    unordered_map <int, set<int>> nti;
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        
+        if(itn.find(index) != itn.end()){
+            int prev = itn[index];
+            nti[prev].erase(index);
+                if(nti[prev].empty())
+                    nti.erase(prev);
+            itn[index] = number;
+        }
+        itn[index] = number;
+        nti[number].insert(index);
+    }
+    
+    int find(int number) {
+        if(nti.find(number) == nti.end())
+            return -1;
+        
+        return *nti[number].begin();
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
