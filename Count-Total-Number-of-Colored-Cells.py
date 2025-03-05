17342c8d1cf9f0f1830dbe192acdeb0f8e92cc73
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-03-06 01:04:37 +0530

    [2579] [Time Beats: 100.00%] [Memory Beats: 8.65%] - LeetPush

--- Count-Total-Number-of-Colored-Cells.py
+++ Count-Total-Number-of-Colored-Cells.py
@@ -1,9 +1,3 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        dp = []
-        dp.append(1)
-
-        for i in range(n-1):
-            dp.append(dp[-1] + 4*(i+1))
-
-        return dp[-1]          
+       return 1 + 2 * n * (n-1)
