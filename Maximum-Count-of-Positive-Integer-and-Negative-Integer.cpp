9078de560404afda034c801e45fefb5ce16288cb
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-03-12 22:40:25 +0530

    [2529] [Time Beats: 100.00%] [Memory Beats: 10.71%] - LeetPush

---
+++ Maximum-Count-of-Positive-Integer-and-Negative-Integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos = 0, neg = 0;
+        for(int i : nums){
+            if(i < 0)
+                neg++;
+            
+            else if(i > 0)
+                pos++;
+        }
+        return max(neg, pos);
+    }
+};
