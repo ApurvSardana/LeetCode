0b79f5489dfe884ce5d6f24d341509eb6d219171
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-05-11 12:12:05 +0530

    [3542] [Time Beats: 68.42%] [Memory Beats: 78.95%] - LeetPush

--- Minimum-Operations-to-Convert-All-Elements-to-Zero.cpp
+++ Minimum-Operations-to-Convert-All-Elements-to-Zero.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n = nums.size(); int ans = 0;
-        vector<pair<int, int>> a;
-        set<int> s{n};
-        for(int i = 0; i<n ; i++){
-            //if(nums[i] == 0) s.insert(i);
-            a.push_back({nums[i], i});
-        }
-
-        sort(a.begin(), a.end());
+        int n = nums.size();
+        stack<int> st; int ans = 0;
 
         for(int i = 0; i<n; i++){
-            if(a[i].first != 0) ans++;
-            s.insert(a[i].second);
-            while(i < n-1 && a[i].first == a[i+1].first){
-                int nextSame = a[i+1].second;
-                int nextSmall = *s.upper_bound(a[i].second);
-
-                if(nextSame > nextSmall) break;
-                //s.insert(a[i].second);
-                i++;
-                s.insert(a[i].second);
+            while(!st.empty() && nums[i]<st.top()){
+                st.pop();
             }
-            //s.insert(a[i].second);
+            
+            if(st.empty() || nums[i]>st.top()){
+                st.push(nums[i]);
+                if(nums[i] != 0) ans++;
+            }
         }
+
         return ans;
     }
 };
