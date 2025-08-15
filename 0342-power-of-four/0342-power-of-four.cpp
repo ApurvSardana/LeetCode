e10d892a128b9d7ffbefa24ee49285f58d62dae2
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-08-15 15:00:06 +0530

    Time: 3 ms (3.39%), Space: 8 MB (18.14%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -2,8 +2,17 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n <= 0) return false;
-        if(n == 1) return true;
+        function<int(int)> hsb = [](int x){
+            int ans = 0;
+            while(x != 0){
+                x >>= 1;
+                ans++;
+            }
 
-        return (n%4 == 0 && isPowerOfFour(n/4));
+            return ans-1;
+        };
+
+        cout << hsb(n) << endl;
+        return n == 1 || (__builtin_popcount(n) == 1 && (hsb(n)%2 == 0));
     }
 };
