97b3c9abe2b4c8c9e1029d5d499fd350a8f6d139
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-11-19 15:23:17 +0530

    [2154] [Time Beats: 100.00%] [Memory Beats: 97.92%] - LeetPush

---
+++ Keep-Multiplying-Found-Values-by-Two.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        flag = True
+        while flag:
+            if original in nums:
+                original *= 2
+            else:
+                flag = False
+        return original
