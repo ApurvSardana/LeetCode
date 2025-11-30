ea4480dcce1cc215ac8c784c3990a1da53524370
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2025-12-01 03:27:19 +0530

    [1590] [Time Beats: 95.40%] [Memory Beats: 7.60%] - LeetPush

--- Make-Sum-Divisible-by-P.cpp
+++ Make-Sum-Divisible-by-P.cpp
@@ -10,23 +10,23 @@
 10        vector<ll> ps(n+1, 0);
 11        for(int i = 0; i < n; i++){
 12            if(nums[i]%p == sum) return 1;
-13            ps[i+1] = ps[i] + nums[i];
-14            cout << ps[i+1] << " ";
+13            ps[i+1] = (ps[i] + nums[i])%p;
+14            // cout << ps[i+1] << " ";
 15        }
 16
 17        // cout << endl;
 18
 19        // cout << sum << endl;
 20
-21        map<ll, ll> mp;
+21        unordered_map<ll, ll> mp;
 22        ll ans = INT_MAX;
 23        for(ll i = 0; i <= n; i++){
-24            if(!mp.empty() && mp.find(((ps[i]%p) + p - sum) % p) != mp.end()){
-25                cout << i << ", " << mp[(((ps[i]%p) - sum + p)%p)] << endl;
-26                ans = min(ans, i-mp[(((ps[i]%p) - sum + p)%p)]);
+24            if(!mp.empty() && mp.find((ps[i] + p - sum) % p) != mp.end()){
+25                // cout << i << ", " << mp[(((ps[i]%p) - sum + p)%p)] << endl;
+26                ans = min(ans, i-mp[((ps[i] - sum + p)%p)]);
 27            }
 28
-29            mp[ps[i]%p] = i;
+29            mp[ps[i]] = i;
 30        }
 31
 32        return (ans >= n) ? -1 : ans;
