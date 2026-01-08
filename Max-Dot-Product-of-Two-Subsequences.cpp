dc8f38e05cdf072781d17cec229698ce574b549a
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2026-01-08 06:51:41 +0530

    [1458] [Time Beats: 76.00%] [Memory Beats: 41.60%] - LeetPush

--- Max-Dot-Product-of-Two-Subsequences.cpp
+++ Max-Dot-Product-of-Two-Subsequences.cpp
@@ -26,30 +26,27 @@
 26        // dp[1][1] = nums1[0]*nums2[0];
 27        for(int j = 1; j <= n2; j++){
 28            for(int i = 1; i <= n1; i++){
-29                dp[i][j] = dp[i-1][j];
-30                for(int k = 1; k <= j; k++){
-31                    dp[i][j] = max(dp[i][j], dp[i-1][k-1] + nums1[i-1]*nums2[k-1]);
-32                    ans = max(ans, dp[i][j]);
-33                }
-34            }
-35        }
-36
-37        // for(int i = 1; i <= n1; i++){
-38        //     for(int j = 1; j <= n2; j++){
-39        //         cout << i << "," << j << " : " << dp[i][j] << endl;
-40        //     }
-41        // }
-42
-43        // cout << endl;
-44
-45        if((all_neg1 && all_pos2) || (all_neg2 && all_pos1)){
-46            return -mn1*mn2;
-47        }
-48        return ans;
-49    }
-50
-51    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-52        return f(nums1,nums2);
-53        // return max(f(nums1, nums2), f(nums2, nums1));
-54    }
-55};
+29                dp[i][j] = max({dp[i-1][j], dp[i][j-1], dp[i-1][j-1], dp[i-1][j-1] + nums1[i-1]*nums2[j-1]});
+30                ans = max(ans, dp[i][j]);
+31            }
+32        }
+33
+34        // for(int i = 1; i <= n1; i++){
+35        //     for(int j = 1; j <= n2; j++){
+36        //         cout << i << "," << j << " : " << dp[i][j] << endl;
+37        //     }
+38        // }
+39
+40        // cout << endl;
+41
+42        if((all_neg1 && all_pos2) || (all_neg2 && all_pos1)){
+43            return -mn1*mn2;
+44        }
+45        return ans;
+46    }
+47
+48    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+49        return f(nums1,nums2);
+50        // return max(f(nums1, nums2), f(nums2, nums1));
+51    }
+52};
