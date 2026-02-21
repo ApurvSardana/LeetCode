a7c4e897075f12e5d36f09c3b545a185388e1762
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2026-02-21 05:56:31 +0530

    [762] [Time Beats: 66.88%] [Memory Beats: 40.29%] - LeetPush

--- Prime-Number-of-Set-Bits-in-Binary-Representation.py
+++ Prime-Number-of-Set-Bits-in-Binary-Representation.py
@@ -1,5 +1,4 @@
 1class Solution:
 2    def countPrimeSetBits(self, left: int, right: int) -> int:
-3        primes = [2,3,5,7,11,13,17,19,23,29,31]
-4        return sum([1 for i in range(left,right+1) if bin(i).count('1') in primes])
-5
+3        return sum([1 for i in range(left,right+1) if bin(i).count('1') in [2,3,5,7,11,13,17,19,23,29,31]])
+4
