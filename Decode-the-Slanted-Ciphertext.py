2979bbe5885e0c409d67a0b8f8e29dfe6207443a
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2026-04-05 03:11:55 +0530

    [2075] [Time Beats: 43.50%] [Memory Beats: 75.97%] - LeetPush

---
+++ Decode-the-Slanted-Ciphertext.py
@@ -0,0 +1,18 @@
+1class Solution:
+2    def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+3        cols = len(encodedText) // rows
+4        # g = [[' ']*cols for _ in range(rows)]
+5
+6        # for i in range(rows):
+7        #     for j in range(cols):
+8        #         g[i][j] = encodedText[i*cols + j]
+9        
+10        ans = ""
+11        for i in range(0,cols):
+12            r,c = 0,i
+13            while r < rows and c < cols:
+14                ans += encodedText[r*cols + c]
+15                r += 1
+16                c += 1
+17        
+18        return ans.rstrip()
