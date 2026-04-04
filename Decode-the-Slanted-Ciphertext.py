4d70299ddc02a6a0eb7c82f18ff39c32e4446787
Apurv <156565225+ApurvSardana@users.noreply.github.com>
2026-04-05 03:14:55 +0530

    [2075] [Time Beats: 58.44%] [Memory Beats: 48.05%] - LeetPush

--- Decode-the-Slanted-Ciphertext.py
+++ Decode-the-Slanted-Ciphertext.py
@@ -7,12 +7,12 @@
 7        #     for j in range(cols):
 8        #         g[i][j] = encodedText[i*cols + j]
 9        
-10        ans = ""
+10        ans = []
 11        for i in range(0,cols):
 12            r,c = 0,i
 13            while r < rows and c < cols:
-14                ans += encodedText[r*cols + c]
+14                ans.append(encodedText[r*cols + c])
 15                r += 1
 16                c += 1
 17        
-18        return ans.rstrip()
+18        return ''.join(ans).rstrip()
